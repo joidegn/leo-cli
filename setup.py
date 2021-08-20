18a95e96b218295e7fa9f60823bc4bf1d571dd8e
Johannes Degn <j@degn.de>
2021-08-20 16:34:20 +0530

    Leo does not require lxml anymore

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from distutils.core import setup
 
 setup(name='leo-cli',
-    version='0.3.5',
+    version='0.3.6',
     description='leo.org command line tool',
     author='Johannes Degn',
     author_email='j@degn.de',
     license="MIT",
     url='https://github.com/JoiDegn/leo-cli',
     scripts=['leo'],
-    install_requires=['beautifulsoup4>=4.3.0', 'lxml>=3.0', 'requests>=1.2.3', 'tabulate'],
+    install_requires=['beautifulsoup4>=4.3.0', 'requests>=1.2.3', 'tabulate'],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
